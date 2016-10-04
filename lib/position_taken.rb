f079cc29edfb3d2e033e92c9b47c71887b1f9782
Ahmed ALFLAHY <lfight522@gmail.com>
2016-10-04 09:56:31 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
-# code your #position_taken? method here!
+def position_taken? (board,position)
+if ((board[position] == "X") || (board[position] == "O"))
+  true
+elsif ((board[position] != "") || (board[position] != " "))
+  false
+elsif ((board[position] != "") || (board[position] != " ")) || (board[position] != "X") || (board[position] != "O")
+end
+end
+
+
+
+
+
+
+
+#  board =[" "," "," "," "," "," "," "," "," "]
