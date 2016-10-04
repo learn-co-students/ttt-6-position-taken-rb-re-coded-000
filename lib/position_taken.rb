bc4f8aba6e6a6565b382eca68d775a98e3347798
Mohammed Hamood <m_alkhalifah88@yahoo.com>
2016-10-04 16:54:12 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board,location)
+  if (board[location]== "X") || (board[location]== "O")
+    true
+  elsif (board[location] != "X") || (board[location] != "O")
+    false
+  elsif (board[location] == "X") || (board[location] == "") || (board[location] == "O") || (board[location] == "")
+  end
+  end
