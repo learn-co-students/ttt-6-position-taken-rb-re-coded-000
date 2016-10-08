b41d776b06dc49096fb42c67f4ecb200145a039f
noven temer <noven5823@gmail.com>
2016-10-08 08:50:12 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,9 @@
 # code your #position_taken? method here!
 require_relative '../lib/position_taken.rb'
-#def position_taken?(board,position)
- #if board[position]== " " || board[position]== "" || board[position]== nil
-  # return false
- #elsif board[position]== "X" || board[position]== "O"
-#   return true
-#   end
-#end
-def position_taken?(board, position)
-  !(board[position].nil? || board[position] == " ")
-  !(board[position])=="X"|| board[position])=="O")
+def position_taken?(board,position)
+ if board[position]== " " || board[position]== "" || board[position]== nil
+   return false
+ elsif board[position]== "X" || board[position]== "O"
+   return true
+   end
 end
